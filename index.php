6cdc2d4d834a38ad2d877a25474b63decea3fef8
Ahsan Chaudhary <89289599+PrincePythoniac@users.noreply.github.com>
2024-05-02 14:02:51 +0500

    Add HTML files

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+  <head>
+      <meta name="viewport" content="width=300"/>
+    <meta charset="utf-8">
+    <meta name="description" content="FFCJ trip to Islamabad">
+    <meta name="keywords" content="trip, ff, ffcj, jhelum, islamabad ">
+  	<meta name="author" content="Ahsan Chaudhary">
+    <title>Fauji Foundation | Welcome</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="icon" href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ75-2YJuQTsyzbovxwM8yUOTmXIiTBU05DPQ&usqp=CAU">
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <div id="branding">
+          <h1><span class="highlight">Fauji</span> Foundation</h1>
+        </div>
+        <nav>
+                  <ul>
+            <li class="current"><a href="index.php">Home</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="locations.html">Locations</a></li>
+            <li><a href="current.html">NEWS</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+
+    <section id="showcase">
+      <div class="container">
+        <h1>A<br>Studious<br> Trip To <br><span class="highlight">Islamabad</span></h1>
+        <br>
+        <p>We're pleased to announced that, At Fauji Foundation Jhelum, We've decided to take students of Senior Section (9th - 12th), on a recreational trip to Islamabad. </p>
+      </div>
+    </section>
+
+    <section id="newsletter">
+      <div class="container">
+        <h1>Subscribe To Our Newsletter</h1>
+        <form>
+          <input type="email" placeholder="Enter Email..." required>
+          <button type="submit" class="button_1">Subscribe</button>
+        </form>
+        *This feature is not functional. Would be implemented in future. 
+      </div>
+    </section>
+
+    <section id="boxes">
+      <div class="container">
+        <div class="box">
+          <img class="pic" src="./img/hii.png">
+          <h3>Dates & Times</h3>
+          <p>The trip would start on Early Morning of Wednesday, 31st January 2024 (Insha'Allah). Timings would be provided to willing students through whatsapp or in school.
+         </p>
+        </div>
+        <div class="box">
+          <img class="pic" src="./img/perm.png">
+          <h3>Permission & Funds</h3>
+          <p>The students willing to go  must submit Rs 1500/- on Tuesday alongwith the Permission letter (would pe provided on Monday) which must be signed by Parents</p>
+        </div>
+        <div class="box">
+          <img class="pic" src="./img/ann.png">
+          <h3>Points to Note</h3>
+          <p class="xyz">1. Uniform is necessary.<br>2. No Mobile Phones Allowed.<br>3. Tickets to Park would be provided.<br>4. Rides are on student's own Expense.<br>5. Food to be brought by Students.<br>6. Any Other information would be provided in respective class groups. </p>
+        </div>
+      </div>
+    </section>
+
+    <footer>
+      <p>A Design by Ahsan Chaudhary<br> Copyright &copy; 2024</p>
+    </footer>
+  </body>
+</html>
